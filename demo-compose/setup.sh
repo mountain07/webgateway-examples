a93d161f577180d14249214610571eddf2510e6b
kdu <kdu@intersystems.com>
2023-05-11 13:49:22 -0400

    Removed SSL, edited container :latest tag Currently IRIS containers crash...

--- demo-compose/setup.sh
+++ demo-compose/setup.sh
@@ -8,9 +8,3 @@ chmod -R 755 webgateway
 docker-compose build
 docker-compose up -d
 
-
-# SSL SETUP - omit if opting out of SSL
-# It takes several seconds for iris to spin up, so wait for it
-# before configuring it for SSL. Adjust sleep time if need be.
-sleep 30
-docker exec iris /bin/sh -c "/iris-shared/configure.sh"
