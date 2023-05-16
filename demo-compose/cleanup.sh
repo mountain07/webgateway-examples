12eb864ef85a5ed774dfce4adb40811547a87d78
kdu <kdu@intersystems.com>
2023-05-16 11:09:48 -0400

    minimum files needed

--- demo-compose/cleanup.sh
+++
@@ -1,8 +0,0 @@
-docker stop iris
-docker rm iris
-rm -rf iris/durable
-
-docker stop webgateway
-docker rm webgateway
-rm -rf webgateway/durable
-
--- demo-compose/setup.sh
+++ demo-compose/setup.sh
@@ -1,10 +1,13 @@
-source parameters.cfg
-
 #; Prior to running the docker-compose, ensure the instances have
 #; read-write permissions to the bindmounts.
+echo "chmoding folders"
 chmod -R 755 iris
 chmod -R 755 webgateway
 
+echo "starting docker image build"
 docker-compose build
+echo "docker images built"
+echo "starting docker compose"
 docker-compose up -d
+echo "all done"
 
